155e46296c00ecbf3b87a1c499ab6986160b07e7
Jiei-Dux <jiei_dux@proton.me>
2024-08-01 17:41:58 +0800

    Update 8 | August 01 2024

--- js/Settings.js
+++
@@ -1,6 +0,0 @@
-// JavaScript to make tooltip follow mouse cursor
-document.addEventListener('mousemove', (event) => {
-    const tooltip = document.getElementById('tooltip');
-    tooltip.style.left = `${event.pageX + 10}px`; // Adjust 10 pixels to the right of the cursor
-    tooltip.style.top = `${event.pageY + 10}px`; // Adjust 10 pixels below the cursor
-});
